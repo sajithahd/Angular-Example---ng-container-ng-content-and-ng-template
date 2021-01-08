ea7304b1bad02ac25e156f52c4a4a5adaadab5af
sajithahd <hdsajitha@gmail.com>
2021-01-09 00:03:12 +0530

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, VERSION } from "@angular/core";
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent  {
-  name = 'Angular-Example---ng-container-ng-content-and-ng-template';
+export class AppComponent {
+  name = "Angular-Example---ng-container-ng-content-and-ng-template";
+
+  items: any[];
+  displayItems: boolean;
+
+  constructor() {
+    this.displayItems = false;
+    this.items = [
+      { name: "sajitha" },
+      { name: "hem" },
+      { name: "ayaan" },
+      { name: "hera" }
+    ];
+  }
 }
